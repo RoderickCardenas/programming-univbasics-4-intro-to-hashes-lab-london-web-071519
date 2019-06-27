a433e6a645efc466d0dc58b0d03033d1f0ccbf21
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-06-27 17:10:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,14 +24,13 @@ end
 
 def update_counting_hash(hash, key)
 
-
+hash.each do|key, value|
+puts "#{key}: #{value}"
+end
 
  if hash[key]
     hash[key]+=1
-    hash.each do|key, value|
-    puts "#{key}: 1"
  else
     hash[key] = 1
- end
  end
 end
